8e03a67c6ae07592d4b9f128e254dcdd39a593dc
Simone Gentili <sensorario@gmail.com>
2019-03-25 02:47:23 +0100

    add the step for feature creation

---
+++ step_feature.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/fatih/color"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type FeatureStep struct{}
+
+func (s *FeatureStep) Execute(c *Context) bool {
+	c.EnterStep()
+
+	cmdName := "git"
+	cmdArgs := []string{"checkout", "master"}
+	if _, err := exec.Command(cmdName, cmdArgs...).Output(); err != nil {
+		if err.Error() == "exit status 128" {
+			fmt.Println(color.RedString("git repository not found"))
+		}
+		os.Exit(1)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print(color.RedString("New feature description: "))
+	featureName, _ := reader.ReadString('\n')
+	featureName = strings.ReplaceAll(featureName, " ", "-")
+	featureName = strings.ReplaceAll(featureName, "\n", "")
+
+	fmt.Print(
+		"Feature: ",
+	)
+
+	featureBranchName := "feature/" + featureName
+	fmt.Println(color.YellowString(featureBranchName))
+
+	cmdStartBranch := "git"
+	arguments := []string{"checkout", "-b", featureBranchName}
+	fmt.Println(arguments)
+	if _, err := exec.Command(cmdStartBranch, arguments...).Output(); err != nil {
+		fmt.Println(color.RedString(err.Error()))
+		os.Exit(1)
+	}
+
+	c.CurrentStep = &FinalStep{}
+
+	return true
+}
+
+func (s *FeatureStep) Stepname() string {
+	return "checkout master"
+}
