6cf890e1511b398ee46df2b31cd311baa44adf78
Simone Gentili <sensorario@gmail.com>
2019-03-26 08:51:24 +0100

    add feature complete step

---
+++ step_feature_complte.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/fatih/color"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type CompleteFeature struct{}
+
+func (s *CompleteFeature) Execute(c *Context) bool {
+	var (
+		cmdOut []byte
+		err    error
+	)
+	cmdName := "git"
+	cmdArgs := []string{"status"}
+	if cmdOut, err = exec.Command(cmdName, cmdArgs...).Output(); err != nil {
+		if err.Error() == "exit status 128" {
+			fmt.Println("\033[1;31mgit repository not found\033[0m")
+		}
+		os.Exit(1)
+	}
+	re := regexp.MustCompile(`On branch [\w\/\#\-]{0,}`)
+	for _, match := range re.FindAllString(string(cmdOut), -1) {
+		branchName := strings.ReplaceAll(match, "On branch ", "")
+	}
+
+	cmdArgs = []string{"checkout", "master"}
+	if _, err := exec.Command(cmdName, cmdArgs...).Output(); err != nil {
+		if err.Error() == "exit status 128" {
+			fmt.Println(color.RedString("git repository not found"))
+		}
+		os.Exit(1)
+	}
+
+	cmdArgs = []string{"merge", "--no-ff", branchName}
+	if _, err := exec.Command(cmdName, cmdArgs...).Output(); err != nil {
+		if err.Error() == "exit status 128" {
+			fmt.Println(color.RedString("git repository not found"))
+		}
+		os.Exit(1)
+	}
+
+	c.CurrentStep = &FinalStep{}
+
+	return true
+}
+
+func (s *CompleteFeature) Stepname() string {
+	return "checkout master"
+}
