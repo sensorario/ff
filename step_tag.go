8897ec356e28427850facb167717f8d083f0b427
Simone Gentili <sensorario@gmail.com>
2019-04-11 01:26:31 +0200

    add new tag command

---
+++ step_tag.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/fatih/color"
+)
+
+type tagStep struct{}
+
+func (s tagStep) Execute(c *context) bool {
+	gitDescribeTags := &gitCommand{
+		c.Logger,
+		[]string{"describe", "--tags"},
+		"cant force tag",
+	}
+
+	cmdOut := gitDescribeTags.Execute()
+
+	fmt.Print("current tag: ", color.GreenString(string(cmdOut)))
+
+	tagName := ""
+
+	branchName := ""
+	re := regexp.MustCompile(`On branch [\w\/\#\-\.]{0,}`)
+	for _, match := range re.FindAllString(string(cmdOut), -1) {
+		branchName = strings.ReplaceAll(match, "On branch ", "")
+	}
+
+	mt := meta{string(cmdOut), branchName}
+
+	tagName = mt.NextMinorTag()
+
+	fmt.Println("next tag:   ", color.GreenString(tagName))
+
+	gitTag := &gitCommand{
+		c.Logger,
+		[]string{"tag", tagName, "-f"},
+		"cant tag",
+	}
+	_ = gitTag.Execute()
+
+	c.CurrentStep = &finalStep{}
+
+	return true
+}
+
+func (s tagStep) Stepname() string {
+	return "force-tag"
+}
