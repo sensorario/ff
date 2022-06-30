0fd7ecd09c7d28c18ef619de477e9a76cb9453d1
Simone Gentili <sensorario@gmail.com>
2022-06-30 09:43:09 +0200

    add todo for next development

--- src/step_commit.go
+++ src/step_commit.go
@@ -11,6 +11,9 @@ type commitStep struct{}
 func (s commitStep) Execute(c *context) bool {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("What is the commit message: ")
+
+    // @todo prefix with current branch type
+
 	text, _ := reader.ReadString('\n')
 	fmt.Println(text)
 
